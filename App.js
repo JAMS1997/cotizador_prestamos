4fcc6e287f4e8c88004c80e268ff11887de03379
Jhon Melendez <jhonmelendez77@gmail.com>
2023-09-21 19:46:00 -0500

    Incio de Cotizador de prestamos
    Se inicia el proyecto de cotizador de presatmos en react native

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import React from 'react';
+import { StyleSheet, Text, View , SafeAreaView, StatusBar, Button} from 'react-native';
+import Form from './src/components/Form'
+import colors from './src/utils/colors'
 
 export default function App() {
+  const [capital, setCapital] = useState(null);
+  const [interest, setInterest] = useState(null);
+  const [months, setMonths] = useState(null);
+
+  const onSubmit = ()=> {
+    console.log('capital=>'+capital);
+    console.log('interes=>'+interest);
+    console.log('Meses=>'+months);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <>
+    <StatusBar bardStyle="light-content"/>
+    <SafeAreaView style={styles.safeArea}>
+      <Text style={styles.titleApp}>Cotizador de prestamos</Text>
+      <Form setCapital={setCapital} setInterest={setInterest} setMonths={setMonths} />
+    </SafeAreaView>
+
+    <View>
+      <Text>Resultado</Text>
     </View>
+
+    <View>
+      <Button title="Enviar" onPress={onSubmit}/>
+      <Text>Footer</Text>
+    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  safeArea:{
+    backgroundColor:colors.PRYMARY_COLOR_DARK ,
+    height:200,
+    borderBottomLeftRadius:30,
+    borderBottomRightRadius:30,
+    alignItems:"center"
   },
+  titleApp:{
+    fontSize:25,
+    fontWeight:"bold",
+    color:'#FFFFFF',
+    marginTop:15
+  }
 });
+
---
+++ src/utils/colors.js
@@ -0,0 +1,4 @@
+export default {
+    PRYMARY_COLOR:"#009803",
+    PRYMARY_COLOR_DARK:"#006691"
+}
